cff334462b558d4b39207ab3a0e2a67d16260f45
samuel210 <samuel210@live.fr>
2025-05-26 17:46:08 +0200

    Adding navbar

---
+++ components/nav-user.tsx
@@ -0,0 +1,65 @@
+"use client"
+import { LogOutIcon, MoreVerticalIcon } from "lucide-react"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Button } from "./ui/button";
+//import { authClient } from "@/lib/auth-client"
+
+export default function NavUser({ user }: {
+    user: {
+        name: string
+        email: string
+        avatar: string
+    }
+}) {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant='ghost'
+                    className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
+                    <Avatar className="h-8 w-8 rounded-lg grayscale">
+                        <AvatarImage src={user.avatar} alt={user.name} />
+                        <AvatarFallback className="rounded-lg">{user.name.split(' ')[0].charAt(0).toLocaleUpperCase() + user.name.split(' ')[1].charAt(0).toLocaleUpperCase()}</AvatarFallback>
+                    </Avatar>
+                    <MoreVerticalIcon className="ml-auto size-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+                className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                side={"bottom"}
+                align="end"
+                sideOffset={4}
+            >
+                <DropdownMenuLabel className="p-0 font-normal">
+                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                        <Avatar className="h-8 w-8 rounded-lg">
+                            <AvatarImage src={user.avatar} alt={user.name} />
+                            <AvatarFallback className="rounded-lg">{user.name.split(' ')[0].charAt(0).toLocaleUpperCase() + user.name.split(' ')[1].charAt(0).toLocaleUpperCase()}</AvatarFallback>
+                        </Avatar>
+                        <div className="grid flex-1 text-left text-sm leading-tight">
+                            <span className="truncate font-medium">{user.name}</span>
+                            <span className="truncate text-xs text-muted-foreground">
+                                {user.email}
+                            </span>
+                        </div>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {/*<DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        <UserCircleIcon />
+                        Account
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                </DropdownMenuGroup>*/}
+                <DropdownMenuItem onClick={async () => {
+                    //await authClient.signOut();
+                }}>
+                    <LogOutIcon />
+                    Se déconnecter
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+};
